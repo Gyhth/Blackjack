c4c76f96c1f5d96a03542b46c7a261f8371cc444
Gyhth <Gyhthy@gmail.com>
2017-08-10 19:19:41 -0400

    Addition of the ability to split
    Added the ability for the user to possess more than one hand (Split) and handle those conditions properly. Just an initial take, not properly tested as yet but seems to work as intended so far.

--- blackjack.js
+++ blackjack.js
@@ -1,17 +1,21 @@
 var BlackjackClass = function() {};
 
 BlackjackClass.prototype = {
-  playerHand: [],
+  playerHand: [
+    []
+  ],
   dealerHand: [],
   playerBet: 0,
   playerCash: 0,
+  hand: 0,
 
   start: function() {
+    this.hand = 0;
     for (var i = 0; i < 4; i++) {
       if (i % 2) {
         this.dealerHand.push(deck.deal());
       } else {
-        this.playerHand.push(deck.deal());
+        this.playerHand[this.hand].push(deck.deal());
       }
     }
   },
@@ -26,29 +30,35 @@ BlackjackClass.prototype = {
 
   stay: function() {
     var state = -1;
-    while (this.sum(this.dealerHand) < 17) {
-      if (notEnoughCards()) {
-        deck.generate();
+    if (this.hand == this.playerHand.length - 1) {
+      while (this.sum(this.dealerHand) < 17) {
+        if (notEnoughCards()) {
+          deck.generate();
+        }
+        this.dealerHand.push(deck.deal());
       }
-      this.dealerHand.push(deck.deal());
-    }
-    if ((this.sum(this.dealerHand) < 22) && (this.sum(this.playerHand) < this.sum(this.dealerHand))) {
-      state = 0;
-    } else if (this.sum(this.playerHand) > 21) {
-      state = 0;
-    } else if (this.sum(this.playerHand) == this.sum(this.dealerHand)) {
-      state = 1;
-    } else if (this.sum(this.playerHand) == 21 && this.playerHand.length == 2) {
-      state = 2;
+      for (var x = 0; x < this.playerHand[i].length; x++) {
+        if ((this.sum(this.dealerHand) < 22) && (this.sum(this.playerHand[x]) < this.sum(this.dealerHand))) {
+          state = 0;
+        } else if (this.sum(this.playerHand[x]) > 21) {
+          state = 0;
+        } else if (this.sum(this.playerHand[x]) == this.sum(this.dealerHand)) {
+          state = 1;
+        } else if (this.sum(this.playerHand[x]) == 21 && this.playerHand[x].length == 2) {
+          state = 2;
+        } else {
+          state = 3;
+        }
+        this.endRound(state);
+      }
     } else {
-      state = 3;
+      this.hand++;
     }
-    this.endRound(state);
   },
 
   hit: function() {
-    this.playerHand.push(deck.deal());
-    if (this.sum(this.playerHand) > 21) {
+    this.playerHand[this.hand].push(deck.deal());
+    if (this.sum(this.playerHand[this.hand]) > 21) {
       this.stay();
     }
   },
@@ -73,6 +83,14 @@ BlackjackClass.prototype = {
       return true;
     }
     return false;
+  },
+
+  split: function() {
+    if ((this.playerHand[this.hand].length == 2) && (this.playerHand[this.hand][0].name == this.playerHand[this.hand][1].name)) {
+      this.playerHand[this.playerHand.length - 1] = this.playerHand[this.hand].splice(0, 1);
+      game.deal(this.playerHand[this.hand]);
+      game.deal(this.playerHand[this.playerHand.length - 1]);
+    }
   },
 };
 
